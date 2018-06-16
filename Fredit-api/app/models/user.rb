a68cfcf7e9bf25f3fc77b28a863e4655f64a0f93
Roman Shorin <roma.shorin@gmail.com>
2018-06-16 14:01:01 +0300

    Add Passport model.

--- Fredit-api/app/models/user.rb
+++ Fredit-api/app/models/user.rb
@@ -5,12 +5,14 @@
 #  id         :integer          not null, primary key
 #  phone      :string
 #  email      :string
-#  status     :integer
+#  status     :integer          default("debtor"), not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
 
 class User < ApplicationRecord
+  has_one :passport
+
   enum status: [:debtor, :creditor]
 
   validates :phone, presence: true
--- Fredit-api/db/schema.rb
+++ Fredit-api/db/schema.rb
@@ -10,10 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_16_102820) do
+ActiveRecord::Schema.define(version: 2018_06_16_104244) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "passports", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "number"
+    t.string "identification"
+    t.string "nationality"
+    t.date "place_of_birth"
+    t.date "date_of_birth"
+    t.date "date_of_issue"
+    t.date "date_of_expire"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_passports_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "phone"
