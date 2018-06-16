1be3daf6e20570e0ce103f77b4168a7d7a68dc51
Dmitry Kremez <dzmity.kremez@itechart-group.com>
2018-06-16 19:01:20 +0300

    Add phone input mask and validation

---
+++ Fredit/src/components/InputMaskBox/index.js
@@ -0,0 +1,46 @@
+import * as React from 'react';
+import TextInputMask from 'react-native-text-input-mask';
+
+// constants
+import { COLORS } from '../../utils/constants';
+
+// styles
+import { styles } from '../InputBox/styles';
+
+export default class InputMaskBox extends React.Component {
+  static defaultProps = {
+    textColor: COLORS.background,
+    color: COLORS.primaryDark,
+    disabledColor: COLORS.primaryDisabled,
+    onChangeText: () => {},
+    disabled: false
+  };
+
+  render() {
+    const {
+      value,
+      color,
+      textColor,
+      onChangeText,
+      disabled,
+      disabledColor,
+      mask,
+      hasError
+    } = this.props;
+
+    return (
+      <TextInputMask
+        value={value}
+        onChangeText={onChangeText}
+        mask={mask}
+        style={[
+          styles.input,
+          {
+            color: textColor,
+            borderColor: !disabled ? color : disabledColor,
+          }
+        ]}
+      />
+    );
+  }
+}
--- Fredit/src/utils/constants.js
+++ Fredit/src/utils/constants.js
@@ -1,19 +1,20 @@
 // screens
-export const WELCOME_SCREEN = "WELCOME_SCREEN";
-export const MAIN_SCREEN = "MAIN_SCREEN";
-export const NEW_TRANSACTION_SCREEN = "NEW_TRANSACTION_SCREEN";
-export const TRANSACTIONS_SCREEN = "TRANSACTIONS_SCREEN";
-export const SETTINGS_SCREEN = "SETTINGS_SCREEN";
+export const WELCOME_SCREEN = 'WELCOME_SCREEN';
+export const MAIN_SCREEN = 'MAIN_SCREEN';
+export const NEW_TRANSACTION_SCREEN = 'NEW_TRANSACTION_SCREEN';
+export const TRANSACTIONS_SCREEN = 'TRANSACTIONS_SCREEN';
+export const SETTINGS_SCREEN = 'SETTINGS_SCREEN';
 
 // colors
 export const COLORS = {
-  background: "#FFFFFF",
-  primaryDark: "#7B1FA2",
-  primary: "#9C27B0",
-  accent: "",
-  primaryDisabled: "#E1BEE7"
+  background: '#FFFFFF',
+  primaryDark: '#455A64',
+  primary: '#607D8B',
+  accent: '',
+  primaryDisabled: '#CFD8DC',
+  textColor: 'rgba(0,0,0, 0.8)'
 };
 
 // constants
-export const SET_LOGGED_IN_ACTION = "SET_LOGGED_IN_ACTION";
-export const SET_TOKEN_ACTION = "SET_TOKEN_ACTION";
+export const SET_LOGGED_IN_ACTION = 'SET_LOGGED_IN_ACTION';
+export const SET_TOKEN_ACTION = 'SET_TOKEN_ACTION';
