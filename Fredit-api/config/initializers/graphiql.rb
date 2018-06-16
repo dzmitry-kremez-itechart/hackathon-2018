6a104b45d87e3ee56ace9211cc9e02a59c780025
Dmitry Kremez <dzmity.kremez@itechart-group.com>
2018-06-16 21:13:56 +0300

    Allow graphiql in prod

--- Fredit-api/config/initializers/graphiql.rb
+++ Fredit-api/config/initializers/graphiql.rb
@@ -1,5 +1,3 @@
-if Rails.env.development?
-  GraphiQL::Rails.config.headers['Authorization'] = -> (_ctx) {
-    "bearer #{ENV['JWT_TOKEN']}"
-  }
-end
+GraphiQL::Rails.config.headers['Authorization'] = -> (_ctx) {
+  "bearer #{ENV['JWT_TOKEN']}"
+}
