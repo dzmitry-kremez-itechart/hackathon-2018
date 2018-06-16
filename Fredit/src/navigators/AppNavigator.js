28b8f497a5ab2578d197c938933931e37444837f
Andrei Andzileuka <andandalex@gmail.com>
2018-06-16 20:16:45 +0300

    refactor navigation

--- Fredit/src/navigators/AppNavigator.js
+++ Fredit/src/navigators/AppNavigator.js
@@ -6,21 +6,34 @@ import MainNavigator from "./MainNavigator";
 
 // screens
 import WelcomeScreen from "../screens/WelcomeScreen";
+import CreditCardScreen from "../screens/CreditCardScreen";
+import PassportScreen from "../screens/PassportScreen";
 
 // constants
-import { WELCOME_SCREEN, MAIN_SCREEN } from "../utils/constants";
+import {
+  WELCOME_SCREEN,
+  MAIN_NAVIGATOR,
+  CREDIT_CARD_SCREEN,
+  PASSPORT_SCREEN
+} from "../utils/constants";
 
 export default createStackNavigator(
   {
     [WELCOME_SCREEN]: {
       screen: ({ navigation }) => (
         <WelcomeScreen
-          openMain={() => navigation.navigate({ routeName: MAIN_SCREEN })}
+          openMain={() => navigation.navigate({ routeName: MAIN_NAVIGATOR })}
         />
       )
     },
-    [MAIN_SCREEN]: {
+    [MAIN_NAVIGATOR]: {
       screen: MainNavigator
+    },
+    [CREDIT_CARD_SCREEN]: {
+      screen: props => <CreditCardScreen {...props} />
+    },
+    [PASSPORT_SCREEN]: {
+      screen: props => <PassportScreen {...props} />
     }
   },
   { headerMode: "none" }
