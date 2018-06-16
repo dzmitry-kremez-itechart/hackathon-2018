a403782db94176ca188c820d59ef3e90d95c48a9
Dmitry Kremez <dzmity.kremez@itechart-group.com>
2018-06-16 17:35:37 +0300

    Add issued contract start

--- Fredit-api/app/graphql/types/query_type.rb
+++ Fredit-api/app/graphql/types/query_type.rb
@@ -3,4 +3,12 @@ class Types::QueryType < Types::BaseObject
   def user
     context[:current_user]
   end
+
+  field :issued_loan_contracts, [Types::LoanContractType], null: true do
+    argument :limit, Integer, required: false
+    argument :offset, Integer, required: false
+  end
+  def issued_loan_contracts(limit: 100, offset: 0)
+    LoanContract.issued.order(created_at: :desc).offset(offset).limit(limit)
+  end
 end
