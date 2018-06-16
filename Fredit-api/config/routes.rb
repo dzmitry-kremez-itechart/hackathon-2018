f4bb28e92711e6eab261576956b307db11c87d42
Roman Shorin <roma.shorin@gmail.com>
2018-06-16 16:45:14 +0300

    Add auth by phone.

--- Fredit-api/config/routes.rb
+++ Fredit-api/config/routes.rb
@@ -4,4 +4,12 @@ Rails.application.routes.draw do
   if Rails.env.development?
     mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
   end
+
+  resources :users, only: :create do
+    collection do
+      get :verify
+      post :verify
+      post :resend
+    end
+  end
 end
