cbb648340e0dbbe48c4c4b29f4c27cd2d94e6ae2
Dmitry Kremez <dzmity.kremez@itechart-group.com>
2018-06-16 14:22:37 +0300

    Add graphql data

--- Fredit-api/app/graphql/types/query_type.rb
+++ Fredit-api/app/graphql/types/query_type.rb
@@ -1,11 +1,7 @@
 class Types::QueryType < Types::BaseObject
-  # Add root-level fields here.
-  # They will be entry points for queries on your schema.
-
-  # TODO: remove me
-  field :test_field, String, null: false,
-    description: "An example field added by the generator"
-  def test_field
-    "Hello World!"
+  field :user, !types[Types::UserType] do
+    resolve -> (obj, args, ctx) {
+      ctx[:current_user]
+    }
   end
 end
--- Fredit-api/db/seeds.rb
+++ Fredit-api/db/seeds.rb
@@ -5,5 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create(phone: '375291112233')
-User.create(phone: '375294445566', status: :creditor)
+debitor = User.find_or_create_by(phone: '375291112233')
+Passport.find_or_create_by(
+  user: debitor,
+  first_name: 'Vasia',
+  last_name: 'Pupkin',
+  number: 'BM123123',
+  identification: '1231231EE3242PS1',
+  nationality: 'Belarus',
+  place_of_birth: 'Minsk',
+  date_of_birth: 20.years.ago,
+  date_of_issue: 2.years.ago,
+  date_of_expire: 3.years.from_now,
+)
+
+creditor = User.find_or_create_by(phone: '375294445566', status: :creditor)
+Passport.find_or_create_by(
+  user: creditor,
+  first_name: 'Ilon',
+  last_name: 'Mask',
+  number: 'BM123123',
+  identification: '1231231EE3242PS1',
+  nationality: 'USA',
+  place_of_birth: 'New York',
+  date_of_birth: 30.years.ago,
+  date_of_issue: 1.years.ago,
+  date_of_expire: 4.years.from_now,
+)
